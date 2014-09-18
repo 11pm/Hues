0cb4bac51eb6805d7bb07e8152e3911393aad010
Alexander Hilmar Arnarson <alexander.hilmar@gmail.com>
2014-09-18 20:22:51 +0000

    added skinpack

--- js/main.js
+++ js/main.js
@@ -1,29 +1,37 @@
 
 var allSongs = [];
 
-for(var i = 0; i < songs.length; i++){
-	allSongs.push(songs[i]);
+for(var i = 0; i < song.length; i++){
+	allSongs.push(song[i]);
 }
-newSong(allSongs[0]);
+
+function newRandomSong(){
+	newSong(allSongs[Math.floor(Math.random() * allSongs.length)]);
+
+}
+newSong(allSongs[7]);
 
 
 function newSong(obj){
-	var songName = obj.songName;
+	buzz.all().stop();
+	var songName = obj["title"];
 	var actions = [];
+	console.log(obj);
 
-	console.log('Playing: ' + songName)
-	var mySound = new buzz.sound('audio/' + songName,{
+	console.log('Playing: ' + obj["-name"]);
+	var mySound = new buzz.sound('audi/' + obj['-name'],{
 		formats: ['mp3']
 	});
 	mySound.play()
 	   	.fadeIn()
 	   	.loop()
 	    .bind( "timeupdate", function() {
-	       var timer = buzz.toTimer( this.getTime() );
-	       //console.log(timer);
-	       if(timer in obj.points){
-	       		newAction(obj, timer);
-	       }
+	       var time = this.getTime();
+	       var duration = this.getDuration();
+	       var percent = buzz.toPercent(time, duration, 1);
+
+	       //console.log(allSongs.length/percent);
+	       //console.log(obj.rhythm);
 	    });
 	updateTemplate(obj, {style: "background-color", fill: "white"});
 	//newSong(allSongs[Math.floor(Math.random() * allSongs.length)])
@@ -42,19 +50,31 @@ function newAction(obj, time){
 	}
 }
 function updateTemplate(obj, bg){
-	$('.songName').html(obj.songName);
+
+	var character = getRandomCharacter()
+	var currentChar = 'img/' + character;
 	$('.main').css(bg.style, bg.fill);
-	var currentChar = 'img/' + getRandomCharacter();
-	console.log(currentChar);
 	$('.main').css('background-image', 'url(' + currentChar + ')');
-}
+
+	var template = Handlebars.compile($('#overlay').html());
+
+	var context = {
+		songName: obj.title,
+		charName: character
+	};
+
+	$('.overlay').html(template(context));
+	}
 function getRandomColor(){
 	var colors = ['#0066cc', '#e52e90', '#7FFF00', '#FFD700'];
 	return colors[Math.floor(Math.random() * colors.length)];
 }
+function addExt(file){
+	return file + '.png';
+}
 function getRandomCharacter(){
-	var chars = ['rei.png', 'taiga.png'];
-	return chars[Math.floor(Math.random() * chars.length)];
+	var chars = ['Aisaka_Taiga', 'Zakuro'];
+	return addExt(chars[Math.floor(Math.random() * chars.length)]);
 }
 function fillMain(){
 	$('.main').height($(window).height());
