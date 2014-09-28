a318af82943c7cb0379dcca3f975d18ee6d6897e
Alexander Hilmar Arnarson <alexander.hilmar@gmail.com>
2014-09-28 04:08:24 +0000

    chenged something

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ $(document).ready(function(){
 		},
 
 		playSong: function(obj){
-			
+			buzz.all().stop();			
 
 			var songName = obj.title;
 
@@ -79,8 +79,8 @@ $(document).ready(function(){
 			
 			song.bind( "timeupdate", function() {
        			
-       			hues.curTime = this.getTime()
-       			setInterval(hues.doBeat(obj, this.getExactDuration(), this.getTime()), 0);
+       			
+       			setInterval(hues.doBeat(obj, this.getExactDuration(), this.getTime()), 20);
 
        			
 
@@ -88,6 +88,7 @@ $(document).ready(function(){
 		},
 
 		doBeat: function(obj, duration, curtime){
+			console.log(arguments)
 			var beatMap = obj.rhythm;
 			var beatIndex = Math.floor(curtime / (duration / beatMap.length))
 			var currentBeat = beatMap[beatIndex];
@@ -118,14 +119,14 @@ $(document).ready(function(){
 			.css('background-image', 'url(' + utilities.parseSong(character) + ')')
 			.css('background-color', color.value);
 
-			$('.songName')
-			.html(obj.title);	
-			$('.charName')
-			.html(character);
-			$('.colorName')
-			.html(color.name);
-			$('.rhythm')
-			.html(txt);
+			var template = Handlebars.compile( $('#template').html() );
+			var context = {
+				colorName: color.name,
+				charName: character,
+				songName: obj.title,
+				rhythm: txt
+			};
+			$('.overlay').empty().append(template(context));
 		}
 	}
 
@@ -140,76 +141,3 @@ $(document).ready(function(){
 		hues.newRandomSong();
 	})
 });
-/*function newRandomSong(){
-	newSong(songs[Math.floor(Math.random() * songs.length)]);
-}
-
-newSong(allSongs[7]);
-
-
-function newSong(obj){
-	buzz.all().stop();
-	var songName = obj["title"];
-	var actions = [];
-	console.log(obj);
-
-	console.log('Playing: ' + obj["-name"]);
-	var mySound = new buzz.sound('audio/' + obj['-name'],{
-		formats: ['mp3']
-	});
-	mySound.play()
-	   	.fadeIn()
-	   	.loop()
-	    .bind( "timeupdate", function() {
-	       var time = this.getTime();
-	       var duration = this.getDuration();
-	       var percent = buzz.toPercent(time, duration, 1);
-
-	      
-	    });
-
-	updateTemplate(obj, {style: "background-color", fill: "white"});
-}
-function newAction(obj, time){
-	var points = obj.points;
-	var action = points[time];
-	var attr  = "background-color";
-	switch(action){
-		case 1:
-			updateTemplate(obj, {style: attr, fill: getRandomColor()});
-			break;
-		case 2:
-			updateTemplate(obj, {style: attr, fill: "black"});
-			break
-	}
-}
-function updateTemplate(obj, bg){
-
-	var character = getRandomCharacter()
-
-	$('.main').css('background-image', 'url(' + parseSong(character) + ')').css(bg.style, bg.fill);
-
-	$('.songName').html(obj.title);
-	t	$('.charName').html(character)
-
-}
-
-function getRandomColor(){
-	var colors = ['#0066cc', '#e52e90', '#7FFF00', '#FFD700'];
-	return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function parseSong(file){
-	return "img/" + file + ".png";
-}
-
-function getRandomCharacter(){
-	return characters[Math.floor(Math.random() * characters.length)];
-}
-
-function fillMain(){
-	$('.main').height($(window).height());
-}
-
-$(window).resize(fillMain);
-$(window).load(fillMain);*/
