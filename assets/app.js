11951dbaee556e4b7e44b46256e8ff94c86f0432
11pm <alexander.hilmar@gmail.com>
2015-06-02 22:33:24 +0000

    started working with promises

--- assets/app.js
+++ assets/app.js
@@ -594,7 +594,7 @@ angular.module('hues').service('utils', function(){
 
 });
 
-angular.module('hues').factory('player', function(ngAudio, utils){
+angular.module('hues').factory('player', function($q, ngAudio, utils){
 
 	return {
 
@@ -610,28 +610,56 @@ angular.module('hues').factory('player', function(ngAudio, utils){
 			this.play(utils.random(this.data.songs));
 
 		},
+		
+		audioPromise: function(song){
+
+			var deferred = $q.defer();
+			console.log('from promise')
+			var audio = ngAudio.load('/assets/audio/' + song.source);
+			if(!audio){
+				deferred.reject('Audio failed to play');
+			}
+			else{
+				audio.loop = true;
+				audio.volume = 0;
+				console.log('playing song')
+				audio.play()
+				console.log(audio)
+				console.log(audio.duration)
+				deferred.resolve(audio);
+			}
+
+			return deferred.promise;
+		},
 
 		play: function(song){
 
-			var song = ngAudio.load('/assets/audio/' + song.source);
-			song.loop = true;
-			song.play();
+			var promise = this.audioPromise(song);
+			console.log('getting song')
+			promise.then(function(audio){
+				console.log(audio.duration)
+			}, function(reason){
+				alert(reason)
+			});
 
 			//set the current playing object
-			this.playing = {
-				song: song,
-				image: utils.random(this.data.images),
-				color: utils.random(this.data.colors)
-			};
+			// this.playing = {
+			// 	song: audio,
+			// 	image: utils.random(this.data.images),
+			// 	color: utils.random(this.data.colors)
+			// };
 
-			//
-			var that = this;
-			setInterval(function(){
-				that.playing.image = utils.random(that.data.images);
-				console.log('setting new image from play()')
-			}, 500);
+			// //
+			// var that = this;
+			// setInterval(function(){
+			// 	console.log(dur)
+			// 	that.playing.image = utils.random(that.data.images);
+			// 	that.playing.color = utils.random(that.data.colors);
+			// }, 500);
 
 		},
+
+		
 
 		resume: function(){
 
@@ -668,10 +696,12 @@ angular.module('hues').controller('HuesController', function($scope, $q, data, p
 
 		$scope.player = player;
 
-		$scope.$watch('player.playing.image', function(newV, oldV){
-			
+		$scope.$watch('player.playing.image', function(){
 			$scope.image = player.playing.image;
+		});
 
+		$scope.$watch('player.playing.color', function(){
+			$scope.color = player.playing.color;
 		});
 
 		$scope.color = player.playing.color;
